3b8d2e3116fc4be7019c9eeeffdfd75144df98d5
ElizabethMM36 <elizabethmathew10d@gmail.com>
2026-06-14 14:11:58 +0400

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1 , rob2 = 0 , 0
+        #then houses that are robbed
+        #[rob1, rob2, n, n+1]
+        for n in nums:
+            # temp indicates the maximum house that can be robbed up until now
+            temp= max(n + rob1 , rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
