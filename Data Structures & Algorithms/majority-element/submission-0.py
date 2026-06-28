df72bc1ebb1192cee23029bc809aebdb67ac8a3f
ElizabethMM36 <elizabethmathew10d@gmail.com>
2026-06-28 18:13:45 +0400

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = defaultdict(int)
+        res = maxCount = 0
+        for i in nums:
+            count[i] += 1
+            if maxCount < count[i]:
+                res = i 
+                maxCount = count[i]
+        return res
+
+            
+        
